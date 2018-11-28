2b21cac78bf3a334ea8da16b8ce3f0c791924e14
Objois <objois.eric@gmail.com>
2018-11-28 09:47:54 +0100

    Initial commit

---
+++ src/main/java/fr/objois/universite/universitefront/UniversitefrontApplication.java
@@ -0,0 +1,12 @@
+package fr.objois.universite.universitefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitefrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversitefrontApplication.class, args);
+    }
+}
